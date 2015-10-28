9196e85ea79ea75924ea7a26e63d8d5d37d77f41
Matthew Swartwout <mwswartwout@gmail.com>
2015-10-27 22:54:57 -0400

    Basic outline of three moves

--- src/interesting_moves.cpp
+++ src/interesting_moves.cpp
@@ -1,1 +1,12 @@
+#include <ros/ros.h>
+#include <my_interesting_moves/interesting_moves.h>
+#include <trajectory_msgs/JointTrajectory.h>
 
+void InterestingMoves::set_move_one(trajectory_msgs::JointTrajectory &des_trajectory) {
+}
+
+void InterestingMoves::set_move_two(trajectory_msgs::JointTrajectory &des_trajectory) {
+}
+
+void InterestingMoves::set_move_three(trajectory_msgs::JointTrajectory &des_trajectory) {
+}
